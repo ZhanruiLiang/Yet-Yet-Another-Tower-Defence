0733b5885d111814a56b8da56a2da7e3c159b8c5
Zhanrui Liang <ray040123@gmail.com>
2011-05-06 02:09:58 +0800

    Redesigned the parser used in Clipper::init().

--- parser.h
+++ parser.h
@@ -8,40 +8,56 @@
 using std::string;
 typedef string::iterator strIter;
 
-struct Section
+class Section
 {
-	strIter name_beg, name_end;
-	strIter cont_beg, cont_end;
-	string getName()
-	{
-		string ans;
-		copy(
-				name_beg,
-				name_end,
-				std::back_insert_iterator< string >(ans)
-			);
-		return ans;
-	}
+	public:
+		Section();
+		const strIter & begin();
+		const strIter & end();
+
+		const strIter & cont_beg();
+		const strIter & cont_end();
+
+		virtual string getName();
+		//this method may be override, 
+		// since the return type may be different
+		string getContent();
+	protected:
+		//TODO:finish it
 };
 
-struct Eql
+class EqlSection: public Section
 {
-	string name;
-	string value;
-	Eql(string name0, string value0):name(name0),value(value0){}
-	Eql();
-}
+	public:
+		EqlSection();
+		string getContent();
+	private:
+		//TODO
+};
 
-bool nextSection(const strIter begin,
-		const strIter end,
-		strIter & current, // will be modified
-		Section & section); // the result will be store here, if succeed
+// The parser class
+class Parser
+{
+	public:
+		Parser();
+		// init, tell the parser where to begin and end
+		void init(strIter begin, strIter end);
+		bool virtual nextSection();
+		Section virtual currentSection();
+		bool isEnd();
+	protected:
+		// TODO
+};
 
-bool nextEql(const strIter begin,
-		const strIter end,
-		strIter & current,
-		Eql & eql);
-
+class EqlParser: public Parser
+{
+	public:
+		EqlParser();
+		bool virtual nextSection();
+		Section virtual currentSection();
+	private:
+		// TODO
+};
 bool decode(const strIter begin,
 		const strIter end,
 		string & result);
